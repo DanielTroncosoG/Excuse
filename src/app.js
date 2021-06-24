1d64b688779650d9ce0694401687cb83355c3e7b
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-06-24 15:59:13 +0000

    primer commit excuse

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#excuse").innerHTML = Excuser();
   console.log("Hello Rigo from the console!");
+};
+
+let Excuser = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoi = Math.floor(Math.random() * who.length);
+  let actioni = Math.floor(Math.random() * action.length);
+  let whati = Math.floor(Math.random() * what.length);
+  let wheni = Math.floor(Math.random() * when.length);
+  return (
+    who[whoi] + " " + action[actioni] + " " + what[whati] + " " + when[wheni]
+  );
 };
